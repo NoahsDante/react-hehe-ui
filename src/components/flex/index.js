44a2bc855d29a52d4610028a5d3f6dad6a252f7f
heheda <15257614673@163.com>
2019-03-25 21:44:55 +0800

    增加 - flex组件 - js3

--- src/components/flex/index.js
+++ src/components/flex/index.js
@@ -1,15 +1,60 @@
+import classNames from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Flex = React.forwardRef(({...props},ref) => {
+const Flex = React.forwardRef((
+  {
+      direction,
+      wrap,
+      justify,
+      align,
+      alignContent,
+      className,
+      children,
+      prefixCls,
+      style,
+      ...props
+  }
+  , ref) => {
 
-    return (<div {...props} ref={ref}>dddd</div>);
+    const wrapCls = classNames(prefixCls, className, {
+        [`${prefixCls}-dir-row`]: direction === 'row',
+        [`${prefixCls}-dir-row-reverse`]: direction === 'row-reverse',
+        [`${prefixCls}-dir-column`]: direction === 'column',
+        [`${prefixCls}-dir-column-reverse`]: direction === 'column-reverse',
+
+        [`${prefixCls}-nowrap`]: wrap === 'nowrap',
+        [`${prefixCls}-wrap`]: wrap === 'wrap',
+        [`${prefixCls}-wrap-reverse`]: wrap === 'wrap-reverse',
+
+        [`${prefixCls}-justify-start`]: justify === 'start',
+        [`${prefixCls}-justify-end`]: justify === 'end',
+        [`${prefixCls}-justify-center`]: justify === 'center',
+        [`${prefixCls}-justify-between`]: justify === 'between',
+        [`${prefixCls}-justify-around`]: justify === 'around',
+
+        [`${prefixCls}-align-start`]: align === 'start',
+        [`${prefixCls}-align-center`]: align === 'center',
+        [`${prefixCls}-align-end`]: align === 'end',
+        [`${prefixCls}-align-baseline`]: align === 'baseline',
+        [`${prefixCls}-align-stretch`]: align === 'stretch',
+
+        [`${prefixCls}-align-content-start`]: alignContent === 'start',
+        [`${prefixCls}-align-content-end`]: alignContent === 'end',
+        [`${prefixCls}-align-content-center`]: alignContent === 'center',
+        [`${prefixCls}-align-content-between`]: alignContent === 'between',
+        [`${prefixCls}-align-content-around`]: alignContent === 'around',
+        [`${prefixCls}-align-content-stretch`]: alignContent === 'stretch',
+    });
+
+
+    return (<div {...props} className={wrapCls} style={style} ref={ref}>dddd</div>);
 });
-Flex.propTypes = {
-
-};
+Flex.propTypes = {};
 
 Flex.defaultProps = {
+    prefixCls: 'hehe-flexbox',
+    align: 'center'
 
 };
 export default Flex
