95b171f74ee8a16ec43a145802b482fef810164d
散开一群逗比 散一群逗比 <15257614673@163.com>
2019-04-21 00:38:10 +0800

    增加 - grid组件2

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -3,3 +3,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './style/grid.less';
+
+const propTypes = {
+    prefixCls: PropTypes.string
+};
+const defaultProps = {
+    prefixCls: 'hehe-grid',
+
+};
+const Grid = React.forwardRef(
+  ({
+       prefixCls,
+       className,
+       data,
+       hasLine,
+       square,
+       activeStyle,
+       activeClassName,
+       ...props
+   }, ref) => {
+
+      let {
+          columnNum,
+          carouselMaxRow,
+          onClick,
+          renderItem,
+          ...restPropsForCarousel
+      } = props;
+      const dataLength = (data && data.length) || 0;
+      let rowCount = Math.ceil(dataLength / columnNum);
+      const wrapCls = classnames(prefixCls, className, {
+          [`${prefixCls}-square`]: square,
+          [`${prefixCls}-line`]: hasLine,
+      });
+
+      return (<div className={wrapCls} ref={ref} {...props} />)
+  });
+
+
+Grid.propTypes = propTypes;
+Grid.defaultProps = defaultProps;
+export default Grid;
--- src/components/grid/index.md
+++ src/components/grid/index.md
@@ -18,11 +18,8 @@ subtitle: 宫格
 | onClick    |   点击每个菜单的回调函数   | (el: Object, index: number): void  | - |
 | columnNum    |   列数     | number  |  `4` |
 | hasLine    |   是否有边框     | boolean  |  `true` |
-| isCarousel    |   是否跑马灯,     | boolean  | `false` |
-| carouselMaxRow    |   如果是跑马灯, 一页跑马灯需要展示的行数   | number  | `2` |
 | renderItem    |   自定义每个 grid 条目的创建函数   | (el, index) => React.Node  | - |
 | square     |   每个格子是否固定为正方形   | boolean | true |
 | activeStyle  | 点击反馈的自定义样式 (设为 false 时表示禁止点击反馈) | {}/false | {} |
 | activeClassName  | 点击反馈的自定义类名 | string |  |
 | itemStyle| 每个格子自定义样式| object|{} |
-`isCarousel = true` 模式时，还可以传递 [carousel](https://mobile.ant.design/components/carousel) 相关的 API。
