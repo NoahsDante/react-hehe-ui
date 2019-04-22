9524f3dce6153d47efdf44111c6b41be1580f2a4
gekunyuan <15257614673@163.com>
2019-04-22 16:31:28 +0800

    增加 - Modal 组件 - alert

--- src/components/modal/alert.js
+++ src/components/modal/alert.js
@@ -1,9 +1,14 @@
 import classnames from 'classnames';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
+import Modal from './modal';
+
 const propTypes = {};
-const defaultProps = {};
+const defaultProps = {
+
+};
 const Alert = React.forwardRef((props, ref) => {
     return (<div></div>);
 });
