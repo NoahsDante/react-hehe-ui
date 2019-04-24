5035a9197f4b1377c1e0f1c28f84a5235d7e6862
散开一群逗比 散一群逗比 <15257614673@163.com>
2019-04-24 22:20:04 +0800

    增加 - Modal组件 Prompt

--- src/components/modal/prompt.js
+++ src/components/modal/prompt.js
@@ -1,12 +1,96 @@
-import classnames from 'classnames';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-const propTypes = {};
-const defaultProps = {};
-const Prompt = React.forwardRef((props, ref) => {
-    return (<div></div>);
-});
-Prompt.propTypes = propTypes;
-Prompt.defaultProps = defaultProps;
+import Modal from './modal';
+
+let Prompt = null;
+const propTypes = {
+    prefixCls: PropTypes.string,
+    actions: PropTypes.array,
+    platform: PropTypes.string
+};
+const defaultProps = {
+    prefixCls: 'hehe-modal',
+    actions: [{text: '确定'}],
+    platform: 'ios',
+    type:'default',
+    defaultValue:'',
+    placeholders:['','']
+};
+const PromptElement = React.forwardRef(
+  ({
+       prefixCls,
+       title,
+       message,
+       actions,
+       platform,
+       onClose,
+       ...props
+   }, ref) => {
+
+      const footer = actions.map((button) => {
+          const orginPress = button.onPress || function () {
+          };
+          button.onPress = () => {
+              const res = orginPress();
+              if (res && res.then) {
+                  res
+                    .then(() => {
+                        onClose();
+                    })
+                    .catch(() => {
+                    });
+              } else {
+                  onClose();
+              }
+          };
+          return button;
+      });
+
+      function onWrapTouchStart(e) {
+          if (!/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
+              return;
+          }
+      }
+
+      return (
+        <Modal
+          visible
+          operation
+          transparent
+          transitionName="hehe-zoom"
+          closable={false}
+          maskClosable
+          footer={footer}
+          onClose={onClose}
+          maskTransitionName="hehe-fade"
+          className={`${prefixCls}-operation`}
+          platform={platform}
+          wrapProps={{onTouchStart: onWrapTouchStart}}
+          {...props}
+          ref={ref}
+        >
+        </Modal>
+      );
+  });
+PromptElement.propTypes = propTypes;
+PromptElement.defaultProps = defaultProps;
+
+Prompt = ({...props}) => {
+    const wrapDiv = document.createElement('div');
+    document.body.append(wrapDiv);
+    const close = () => {
+        ReactDOM.unmountComponentAtNode(wrapDiv);
+        if (wrapDiv && wrapDiv.parentNode) {
+            wrapDiv.parentNode.removeChild(wrapDiv);
+        }
+    };
+    ReactDOM.render(<PromptElement onClose={close} {...props}/>, wrapDiv)
+    return {
+        close
+    };
+};
+
+
 export default Prompt;
