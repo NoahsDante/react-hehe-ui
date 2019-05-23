49477d23993c0a9161efbd1a58e565ae8f33ea53
散开一群逗比 散一群逗比 <15257614673@163.com>
2019-05-23 23:51:40 +0800

    buttons - 单元测试2

--- src/components/buttons/__tests__/index.test.js
+++ src/components/buttons/__tests__/index.test.js
@@ -1,6 +1,7 @@
-
+import {render, mount} from 'enzyme';
+import {renderToJson} from 'enzyme-to-json';
+import Button from '../index';
 describe('Button', () => {
-    //
     it('trigger event correctly', () => {
         // todos: write test!
         expect(true).toBe(true);
