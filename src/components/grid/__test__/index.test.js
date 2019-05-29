43bf36a517d8fe6fd6b03147c9aad0c2b067de6f
散开一群逗比 散一群逗比 <15257614673@163.com>
2019-05-29 22:34:19 +0800

    Grid - 单元测试

---
+++ src/components/grid/__test__/index.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {render, mount, shallow} from 'enzyme';
+import {renderToJson} from 'enzyme-to-json';
+import Grid from '../index';
+
+describe('Grid', () => {
+    const prefixCls = '.hehe-grid';
+    const data = Array.from(new Array(9)).map((_val, i) => ({
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+        text: `name${i}`,
+    }));
+    it('Should output a Grid', () => {
+        const wrapper = render(<Grid data={data}/>);
+        expect(renderToJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.is(prefixCls)).toBe(true);
+    });
+    it('Should set prop columnNum', () => {
+        const wrapper = render(<Grid data={data} columnNum={3}/>);
+        expect(renderToJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.find('.hehe-flexbox').first().find(`${prefixCls}-item`)).toHaveLength(3);
+    });
+    it('Should set prop square', () => {
+        const wrapper = render(<Grid data={data} square={false}/>);
+        expect(renderToJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.is(`${prefixCls}-not-square`)).toBe(true);
+    });
+    it('Should call click callback', () => {
+        const clickHandle = jest.fn();
+        const wrapper = shallow(<Grid data={data} onClick={clickHandle}/>);
+        wrapper.find(`${prefixCls}-item`).first().simulate('click');
+        expect(clickHandle).toBeCalled();
+    });
+
+});
