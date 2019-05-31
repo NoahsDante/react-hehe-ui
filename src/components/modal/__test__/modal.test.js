ca3bc5eb32fcccd8fd18c63a28ebf3fcf7e79b0f
散开一群逗比 散一群逗比 <15257614673@163.com>
2019-06-01 00:04:39 +0800

    增加 - modal单元测试

---
+++ src/components/modal/__test__/modal.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dialog from 'rmc-dialog';
+import {render, mount, shallow} from 'enzyme';
+import {renderToJson} from 'enzyme-to-json';
+import Modal from '../modal';
+import App from "../../../App";
+
+describe('Modal', () => {
+    const prefixCls = '.hehe-modal';
+    let focusableContainer;
+
+    beforeEach(() => {
+        focusableContainer = document.createElement('div');
+        document.body.appendChild(focusableContainer);
+    });
+
+    afterEach(() => {
+        ReactDOM.unmountComponentAtNode(focusableContainer);
+        document.body.removeChild(focusableContainer);
+    });
+    it('Should out a Modal', () => {
+        const wrapper = mount(<Modal visible={true}/>);
+        expect(renderToJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.find(`${prefixCls}-content`)).toHaveLength(1);
+    });
+    it('Should set props', () => {
+        const afterCloseClickHandle = jest.fn();
+        const closeClickHandle = jest.fn(() => {
+            afterCloseClickHandle()
+        });
+        const footerClickHandle = jest.fn(() => {
+            closeClickHandle()
+        });
+        const wrapper = mount(<Modal
+          visible={true}
+          onClose={closeClickHandle}
+          title="Title"
+          footer={[{
+              text: 'Ok', onPress: footerClickHandle
+          }]}
+          afterClose={afterCloseClickHandle}
+        />);
+        wrapper.find(`${prefixCls}-button`).simulate('click');
+        expect(footerClickHandle).toBeCalled();
+        expect(wrapper.find(`${prefixCls}-title`)).toHaveLength(1);
+        expect(wrapper.find(`${prefixCls}-button`)).toHaveLength(1);
+        expect(closeClickHandle).toBeCalled();
+        expect(afterCloseClickHandle).toBeCalled();
+    });
+});
