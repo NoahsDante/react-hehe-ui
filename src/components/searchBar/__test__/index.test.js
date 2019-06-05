2c735dfae3e96622ab7bd36409a7a71eaa4a36da
散开一群逗比 散一群逗比 <15257614673@163.com>
2019-06-05 22:51:33 +0800

    增加 - searchBar单元测试

---
+++ src/components/searchBar/__test__/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {render, mount, shallow} from 'enzyme';
+import {renderToJson} from 'enzyme-to-json';
+import SearchBar from '../index';
+
+describe('SearchBar', () => {
+    const prefixCls = '.hehe-search';
+
+    it('Should out a SearchBar', () => {
+        const wrapper = render( <SearchBar placeholder="Search" maxLength={8} />);
+        expect(renderToJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.is(prefixCls)).toBe(true);
+    });
+    it('Should autoFocus',() => {
+        const focusHandle = jest.fn();
+        const wrapper = mount( <SearchBar placeholder="自动获取光标" onFocus={focusHandle} />);
+        wrapper.find('input[type="search"]').simulate('focus');
+        expect(focusHandle).toBeCalled();
+
+    });
+});
